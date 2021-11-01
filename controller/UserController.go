ac83f359be777cda0c0242d6c9e4aa776a87f3f3
CR <931009686@qq.com>
2021-11-02 02:38:50 +0800

    feat: jwt配合中间件用户认证

--- controller/UserController.go
+++ controller/UserController.go
@@ -92,7 +92,12 @@ func Login(ctx *gin.Context) {
 	}
 
 	// 发放token
-	token := "11"
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
+		log.Printf("token generate error : %v", err)
+		return
+	}
 
 	// 返回结果
 	ctx.JSON(200, gin.H{
@@ -100,6 +105,12 @@ func Login(ctx *gin.Context) {
 		"data": gin.H{"token": token},
 		"msg":  "登录成功",
 	})
+}
+
+func Info(ctx *gin.Context) {
+	user, _ := ctx.Get("user")
+
+	ctx.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": user}})
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
---
+++ middleware/AuthMiddleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"cr.com/ginessential/common"
+	"cr.com/ginessential/model"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// 获取authorization header
+		tokenString := ctx.GetHeader("Authorization")
+
+		// validate token format
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		tokenString = tokenString[7:]
+
+		token, claims, err := common.ParseToken(tokenString)
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		// 验证通过后获取claims中的userId
+		userId := claims.UserId
+		DB := common.GetDB()
+		var user model.User
+		DB.First(&user, userId)
+
+		// 用户
+		if user.ID == 0 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"code": 401, "msg": "权限不足"})
+			ctx.Abort()
+			return
+		}
+
+		// 用户存在 将user信息写入上下文
+		ctx.Set("user", user)
+
+		ctx.Next()
+	}
+}
